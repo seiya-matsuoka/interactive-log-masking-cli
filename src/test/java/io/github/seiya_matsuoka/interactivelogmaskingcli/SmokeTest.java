426958c128d7d0a42a009a6550d3494d28dff16c
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2026-01-01 21:20:30 +0900

    test: JUnitのSmokeTestを作成

---
+++ src/test/java/io/github/seiya_matsuoka/interactivelogmaskingcli/SmokeTest.java
@@ -0,0 +1,14 @@
+package io.github.seiya_matsuoka.interactivelogmaskingcli;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/** JUnit の動作確認用スモークテスト。 */
+public class SmokeTest {
+
+  @Test
+  void junit_works() {
+    assertTrue(true);
+  }
+}
