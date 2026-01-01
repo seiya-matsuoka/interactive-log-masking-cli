c39908b7ca201e782bf9d18e53330ebf333b3d30
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2026-01-01 21:20:15 +0900

    feat: エントリポイントのAppを作成

---
+++ src/main/java/io/github/seiya_matsuoka/interactivelogmaskingcli/App.java
@@ -0,0 +1,16 @@
+package io.github.seiya_matsuoka.interactivelogmaskingcli;
+
+/** interactive-log-masking-cli のエントリポイント。 */
+public final class App {
+
+  private App() {}
+
+  /**
+   * アプリケーションを起動する。
+   *
+   * @param args コマンドライン引数
+   */
+  public static void main(String[] args) {
+    System.out.println("interactive-log-masking-cli: 起動しました");
+  }
+}
